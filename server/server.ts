d247b682f4c897771a2b81a9222dc2d2cf90ef80
Valentin LE FLEM <leflem.valentin@gmail.com>
2024-06-24 23:29:42 +0200

    exploration: init

---
+++ server/server.ts
@@ -0,0 +1,16 @@
+Deno.serve(async (req) => {
+  console.log("Method:", req.method);
+
+  const url = new URL(req.url);
+  console.log("Path:", url.pathname);
+  console.log("Query parameters:", url.searchParams);
+
+  console.log("Headers:", req.headers);
+
+  if (req.body) {
+    const body = await req.text();
+    console.log("Body:", body);
+  }
+
+  return new Response("Hello, World!");
+});
---
+++ withConfiguration/main.ts
@@ -0,0 +1,3 @@
+import { assertEquals } from "std/assert/mod.ts";
+
+assertEquals(1, 2);
